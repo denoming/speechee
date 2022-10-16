c7b6f2bedb770044fe9b22058168a3ed786d1929
Denys Asauliak <denoming@gmail.com>
2022-10-16 21:46:17 +0300

    print used toolchain file

--- cmake/ProjectConfigs.cmake
+++ cmake/ProjectConfigs.cmake
@@ -4,6 +4,10 @@ if (EXISTS $ENV{HOME}/.local)
     list(APPEND CMAKE_PREFIX_PATH $ENV{HOME}/.local)
 endif()
 
+if (DEFINED CMAKE_TOOLCHAIN_FILE)
+    message(STATUS "Toolchain file ${CMAKE_TOOLCHAIN_FILE}")
+endif()
+
 include(BuildType)
 include(BuildLocation)
 include(BuildOptions)
