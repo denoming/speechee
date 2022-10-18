5f31d439d71db0e62bf74407db8e6421f0f17fb0
Denys Asauliak <denoming@gmail.com>
2022-10-18 21:32:38 +0300

    add test main with logs initialization

---
+++ tests/common/src/TestMain.cpp
@@ -0,0 +1,13 @@
+#include "speech/LoggerInitializer.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace jar;
+
+int
+main(int argc, char* argv[])
+{
+    LoggerInitializer::initialize();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
