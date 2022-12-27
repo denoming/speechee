edfe470a9c5df1b2778af7f09e46586af95fe1f7
Denys Asauliak <denoming@gmail.com>
2022-12-27 19:25:47 +0200

    improve synthesize test scripts

--- scripts/synthesizeText.sh
+++ scripts/synthesizeText.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-source dbus-env
+PROJECT_DIR=$(dirname "$(dirname "$(realpath -s $0)")")
+
+source $PROJECT_DIR/scripts/dbus-env
 
 TEXT=${1:-"Hello World"}
 LANG=${2:-"en-US"}
+
+if ! pgrep -x "jarvis-speaker" > /dev/null
+then
+    echo -e "Error:"
+    echo -e "J.A.R.V.I.S Speaker process is not active"
+    exit 1
+fi
 
 command -v dbus-send > /dev/null
 if [ $? != 0 ]; then
