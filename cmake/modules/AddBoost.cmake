4d63c553a30a6686e7e5fa2c2fc34f382da9e737
Denys Asauliak <denoming@gmail.com>
2023-02-19 23:32:49 +0200

    bump boost version

--- cmake/modules/AddBoost.cmake
+++ cmake/modules/AddBoost.cmake
@@ -1,6 +1,6 @@
 if(Boost_DEBUG)
     message(STATUS "Boost debug mode is active")
-    find_package(Boost 1.78.0 REQUIRED COMPONENTS ALL)
+    find_package(Boost 1.81.0 REQUIRED COMPONENTS ALL)
 else()
-    find_package(Boost 1.78.0 REQUIRED COMPONENTS program_options system)
+    find_package(Boost 1.81.0 REQUIRED COMPONENTS program_options system)
 endif()
