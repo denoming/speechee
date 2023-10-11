6d965a3916f0d5fc4318ef11cd15e1ad67379024
Denys Asauliak <denoming@gmail.com>
2023-10-11 20:56:51 +0300

    get mosquitto from local vcpkg

--- cmake/modules/AddMosquitto.cmake
+++ cmake/modules/AddMosquitto.cmake
@@ -1,9 +1,3 @@
-find_package(Mosquitto REQUIRED)
+find_package(PkgConfig)
 
-if(Mosquitto_FOUND)
-    set(_TARGET_NAME Mosquitto)
-    add_library(${_TARGET_NAME} SHARED IMPORTED GLOBAL)
-    add_library(${_TARGET_NAME}::${_TARGET_NAME} ALIAS ${_TARGET_NAME})
-    set_target_properties(${_TARGET_NAME} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${Mosquitto_INCLUDE_DIR})
-    set_target_properties(${_TARGET_NAME} PROPERTIES IMPORTED_LOCATION ${Mosquitto_LIBRARY})
-endif()
+pkg_check_modules(Mosquitto REQUIRED IMPORTED_TARGET libmosquitto)
