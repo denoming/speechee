5dab4597ccdf829f8c254cab8f9df6919ad0d376
Denys Asauliak <denoming@gmail.com>
2024-11-24 21:27:05 +0200

    fix: use find config

--- cmake/modules/AddBoost.cmake
+++ cmake/modules/AddBoost.cmake
@@ -1,2 +1,6 @@
 set(Boost_NO_WARN_NEW_VERSIONS ON)
-find_package(Boost 1.81.0 REQUIRED COMPONENTS json)
+set(Boost_NO_BOOST_CMAKE ON)
+find_package(Boost 1.81.0
+    REQUIRED
+    COMPONENTS json
+    CONFIG)
