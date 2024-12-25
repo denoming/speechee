ed432b4956b8f3fa13eec09ee746f8cf3b7ca28e
Denys Asauliak <denoming@gmail.com>
2024-12-25 21:21:10 +0200

    fix: rm unused type streaming

--- src/speaker/include/speaker/Types.hpp
+++ src/speaker/include/speaker/Types.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <ostream>
-
 namespace jar {
 
 enum class PlayState { Null, Idle, Busy, Error };
-
-std::ostream& operator<<(std::ostream& s, PlayState in);
 
 } // namespace jar
