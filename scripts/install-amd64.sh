cd62afeff79dc42ed1482c4eb5486dbdadff4c00
Denys Asauliak <denoming@gmail.com>
2025-08-29 21:48:29 +0300

    chore: use apache license
    [no ci]

--- scripts/install-amd64.sh
+++ scripts/install-amd64.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright 2025 Denys Asauliak
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ONNX_URL=https://github.com/microsoft/onnxruntime/releases/download/v1.21.0/onnxruntime-linux-x64-1.21.0.tgz
 GSLL_URL=https://github.com/gsl-lite/gsl-lite/archive/refs/tags/v0.42.0.tar.gz
 VOXER_URL1=https://github.com/denoming/voxer/releases/download/v0.2.6/libvoxer_0.2.6_amd64.deb
